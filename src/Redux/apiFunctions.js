e5cd10eaefaa8d629230538e6ab241d99396e076
unknown <saied2421998@gmail.com>
2022-04-20 11:47:20 +0200

    finish app structure

---
+++ src/Redux/apiFunctions.js
@@ -0,0 +1,3 @@
+const COUNTRY_LOAD = 'covid-statistics/countries/COUNTRY_LOAD'
+const COUNTRY_LOADED = 'covid-statistics/countries/COUNTRY_LOADED'
+const COUNTRY_LOAD_FAILED = 'covid-statistics/countries/COUNTRY_LOAD_FAILED'
